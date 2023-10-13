eae67e9133bc6062c8a6863917e7f64a3b81e192
Anatoly Dudko <anatoly_dudko@icloud.com>
2023-10-13 20:44:01 +0200

    leetcode refactor

---
+++ leetcode/src/com/leetcode/task/easy/remove_duplicates_from_sorted_array/Solution.java
@@ -0,0 +1,19 @@
+package com.leetcode.task.easy.remove_duplicates_from_sorted_array;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates(new int[]{1,1,2}));
+        System.out.println(removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4}));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
