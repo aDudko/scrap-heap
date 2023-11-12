a95f5582e980a7a284fe287240fafb3124aa5e50
Anatoly Dudko <anatoly_dudko@icloud.com>
2023-11-12 15:59:04 +0100

    javarush: Java Syntax Pro. Level 7

---
+++ java_rush/JavaRushTasks2023/javarush-project/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+    }
+
+    public static void universalMethod(int value){
+    }
+    public static void universalMethod(long value){
+    }
+    public static void universalMethod(double value){
+    }
+    public static void universalMethod(float value){
+    }
+    public static void universalMethod(String value){
+    }
+    public static void universalMethod(boolean value){
+    }
+    public static void universalMethod(short value){
+    }
+    public static void universalMethod(Object value){
+    }
+    public static void universalMethod(int[] value){
+    }
+    public static void universalMethod(double[] value){
+    }
+
+}
