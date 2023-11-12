7624e0ede8249dfed9d500956f50224dc27f10ec
Anatoly Dudko <anatoly_dudko@icloud.com>
2023-11-12 19:44:28 +0100

    javarush: Java Syntax Pro. Level 8

---
+++ java_rush/JavaRushTasks2023/javarush-project/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String str1 = new String("");
+        String str2 = new String("");
+        String str3 = new String("");
+        String str4 = new String("");
+        String str5 = new String("");
+        String str6 = new String("");
+        String str7 = new String("");
+        String str8 = new String("");
+        String str9 = new String("");
+        String str0 = new String("");
+        int[] array1 = new int[1];
+        int[] array2 = new int[1];
+        int[] array3 = new int[1];
+        int[] array4 = new int[1];
+        int[] array5 = new int[1];
+        Scanner scanner1 = new Scanner("");
+        Scanner scanner2 = new Scanner("");
+    }
+}
