2d3a8db18a85a7065be995b4178269df78e910ad
Anatoly Dudko <anatoly_dudko@icloud.com>
2023-11-19 19:04:17 +0100

    javarush: Java Syntax Pro. Level 13

---
+++ java_rush/JavaRushTasks2023/javarush-project/src/com/javarush/task/pro/task12/task1206/Solution.java
@@ -0,0 +1,49 @@
+package com.javarush.task.pro.task12.task1206;
+
+/* 
+Анализ строк
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "Думаю, это будет новой фичей." +
+                "Только не говорите никому, что она возникла случайно.";
+
+        System.out.println("Количество цифр в строке : " + countDigits(string));
+        System.out.println("Количество букв в строке : " + countLetters(string));
+        System.out.println("Количество пробелов в строке : " + countSpaces(string));
+    }
+
+    public static int countDigits(String string) {
+        int result = 0;
+        char[] charArray = string.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            if (Character.isDigit(charArray[i])) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public static int countLetters(String string) {
+        int result = 0;
+        char[] charArray = string.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            if (Character.isLetter(charArray[i])) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public static int countSpaces(String string) {
+        int result = 0;
+        char[] charArray = string.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            if (Character.isWhitespace(charArray[i])) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
