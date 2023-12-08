4500969931b3d7f5918a17365c59ed880e030e0f
Anatoly Dudko <anatoly_dudko@icloud.com>
2023-12-08 21:54:48 +0100

    javarush: Java Core. Level 2

---
+++ java_rush/JavaRushTasks2023/javarush-project/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Integer i = 0;
+        print(1);
+        print(i);
+    }
+
+    public static void print(int i) {
+        System.out.println(i);
+    }
+
+    public static void print(Integer i) {
+        System.out.println(i);
+    }
+}
