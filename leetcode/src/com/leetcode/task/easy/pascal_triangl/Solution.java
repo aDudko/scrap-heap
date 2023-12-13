1b262ddb1b40652d2a9c0f1fc7a17bf06b3f7867
Anatoly Dudko <anatoly_dudko@icloud.com>
2023-12-13 11:17:23 +0100

    leetcode: pascal's_triangl

---
+++ leetcode/src/com/leetcode/task/easy/pascal_triangl/Solution.java
@@ -0,0 +1,32 @@
+package com.leetcode.task.easy.pascal_triangl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(generate(5));
+    }
+
+    public static List<List<Integer>> generate(int numRows) {
+        var row = new ArrayList<>(List.of(1));
+        List<List<Integer>> result = new ArrayList<>(List.of(row));
+        if (numRows == 1) return result;
+
+        row = new ArrayList<>(List.of(1, 1));
+        result.add(row);
+
+        for (var i = 2; i < numRows; i++) {
+            var newRow = new ArrayList<>(List.of(1));
+
+            for (var j = 0; j + 1 < row.size(); j++) {
+                newRow.add(row.get(j) + row.get(j + 1));
+            }
+
+            newRow.add(1);
+            result.add(newRow);
+            row = newRow;
+        }
+        return result;
+    }
+}
