432a8d8dce1555481eda5d2499852742230dd4ac
Anatoly Dudko <anatoly_dudko@icloud.com>
2023-12-17 01:11:22 +0100

    leetcode: kth_largest_element_in_an_array

---
+++ leetcode/src/com/leetcode/task/medium/kth_largest_element_in_an_array/Solution.java
@@ -0,0 +1,37 @@
+package com.leetcode.task.medium.kth_largest_element_in_an_array;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(findKthLargest(new int[]{3,2,1,5,6,4}, 2));          // 5
+        System.out.println(findKthLargest(new int[]{3,2,3,1,2,4,5,5,6}, 4));    // 4
+    }
+
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int num : nums) {
+            pq.add(num);
+        }
+        while (--k > 0) {
+            pq.poll();
+        }
+        return pq.peek();
+    }
+
+//    public static int findKthLargest(int[] nums, int k) {
+//        int max = nums[0], index = 0;
+//        for (int i = 1; i < k; i++) {
+//            for (int j = 0; j < nums.length; j++) {
+//                if (nums[j] > max) {
+//                    max = nums[j];
+//                    index = j;
+//                }
+//            }
+//            nums[index] = Integer.MIN_VALUE;
+//            max = Integer.MIN_VALUE;
+//        }
+//        return Arrays.stream(nums).max().getAsInt();
+//    }
+}
---
+++ leetcode/src/com/leetcode/task/medium/kth_largest_element_in_an_array/readme.md
@@ -0,0 +1,18 @@
+<h4>Given an integer array <code>nums</code> and an integer <code>k</code>, return <i>the <code>k<sup>th</sup></code> largest element in the array</i>.</h4>
+
+<p>Note that it is the <code>k<sup>th</sup></code> largest element in the sorted order, not the <code>k<sup>th</sup></code> distinct element.</p>
+<p>Can you solve it without sorting?</p>
+
+<h6>Example 1:</h6>
+<p><b>Input:</b> <code>nums = [3,2,1,5,6,4], k = 2</code></p>
+<p><b>Output:</b> <code>5</code></p>
+
+<h6>Example 2:</h6>
+<p><b>Input:</b> <code>nums = [3,2,3,1,2,4,5,5,6], k = 4</code></p>
+<p><b>Output:</b> <code>4</code></p>
+
+<h5>Constraints:</h5>
+<ul>
+<li><code>1 <= k <= nums.length <= 10<sup>5</sup></code></li>
+<li><code>-10<sup>4</sup> <= nums[i] <= 10<sup>4</sup></code></li>
+</ul>
