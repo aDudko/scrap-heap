c8bc829660608127053c5af117330488f981d905
Anatoly Dudko <anatoly_dudko@icloud.com>
2023-12-20 00:28:29 +0100

    leetcode: make_array_zero_by_subtracting_equal_amounts

---
+++ leetcode/src/com/leetcode/task/easy/make_array_zero_by_subtracting_equal_amounts/Solution.java
@@ -0,0 +1,27 @@
+package com.leetcode.task.easy.make_array_zero_by_subtracting_equal_amounts;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(minimumOperations(new int[]{1,5,0,3,5}));  // 3
+        System.out.println(minimumOperations(new int[]{0}));          // 0
+    }
+
+    // The count of unique elements is equal to the count of operations
+    public static int minimumOperations(int[] nums) {
+        return (int) Arrays.stream(nums).filter(i -> i != 0).distinct().count();
+    }
+
+//    public static int minimumOperations(int[] nums) {
+//        Set<Integer> set = new HashSet<>();
+//        for (int num : nums) {
+//            if (num != 0) {
+//                set.add(num);
+//            }
+//        }
+//        return set.size();
+//    }
+}
