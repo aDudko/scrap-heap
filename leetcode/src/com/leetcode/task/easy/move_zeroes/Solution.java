bacac4ced4a1c372ff3a856f344288012fddcf83
Anatoly Dudko <anatoly_dudko@icloud.com>
2023-12-23 20:36:35 +0100

    leetcode: move_zeroes

---
+++ leetcode/src/com/leetcode/task/easy/move_zeroes/Solution.java
@@ -0,0 +1,23 @@
+package com.leetcode.task.easy.move_zeroes;
+
+public class Solution {
+    public static void main(String[] args) {
+        moveZeroes(new int[]{0,1,0,3,12});  // [1,3,12,0,0]
+        moveZeroes(new int[]{0});           // [0]
+    }
+
+    public static void moveZeroes(int[] nums) {
+        int i = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                nums[i] = num;
+                i++;
+            }
+        }
+        while (i < nums.length) {
+            nums[i] = 0;
+            i++;
+        }
+//        System.out.println(Arrays.toString(nums));
+    }
+}
