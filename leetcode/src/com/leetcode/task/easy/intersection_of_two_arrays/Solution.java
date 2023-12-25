df745a2c97bcc7264b1155751038965de4076376
Anatoly Dudko <anatoly_dudko@icloud.com>
2023-12-25 01:09:42 +0100

    leetcode: intersection_of_two_arrays

---
+++ leetcode/src/com/leetcode/task/easy/intersection_of_two_arrays/Solution.java
@@ -0,0 +1,24 @@
+package com.leetcode.task.easy.intersection_of_two_arrays;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(intersection(new int[]{1,2,2,1}, new int[]{2,2})));     // [2]
+        System.out.println(Arrays.toString(intersection(new int[]{4,9,5}, new int[]{9,4,9,8,4}))); // [9,4]
+    }
+
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> result = new HashSet<>();
+        for (int j : nums1) {
+            for (int k : nums2) {
+                if (j == k) {
+                    result.add(j);
+                }
+            }
+        }
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+}
---
+++ leetcode/src/com/leetcode/task/easy/intersection_of_two_arrays/readme.md
@@ -0,0 +1,20 @@
+<h3>Intersection of Two Arrays</h3>
+
+<p>Given two integer arrays <code>nums1</code> and <code>nums2</code>, return <i>an array of their intersection</i>. 
+Each element in the result must be <b>unique</b> and you may return the result in <b>any order</b>.</p>
+
+<h6>Example 1:</h6>
+<p><b>Input:</b> <code>nums1 = [1,2,2,1], nums2 = [2,2]</code></p>
+<p><b>Output:</b> <code>[2]</code></p>
+
+<h6>Example 2:</h6>
+<p><b>Input:</b> <code>nums1 = [4,9,5], nums2 = [9,4,9,8,4]</code></p>
+<p><b>Output:</b> <code>[9,4]</code></p>
+<p><b>Explanation:</b> <code>[4,9]</code> is also accepted.</p>
+
+
+<h5>Constraints:</h5>
+<ul>
+    <li><code>1 <= nums1.length, nums2.length <= 1000</code></li>
+    <li><code>0 <= nums1[i], nums2[i] <= 1000</code></li>
+</ul>
