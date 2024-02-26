6ae57b6dd4463e19d55ddd1ee8eb27e3c99baca6
Anatoly Dudko <anatoly_dudko@icloud.com>
2024-02-26 02:21:00 +0100

    udemy: advanced_javascript.solid

--- udemy/advanced_javascript/app/solid.js
+++ udemy/advanced_javascript/app/solid.js
@@ -9,30 +9,105 @@
  * I - принцип разделения интерфейса
  * D - принцип инверсии зависимостей
  */
-// === Принцип единой ответственности ===
-class Character {
-    #inventory = [];
-    #health = 10;
+// // === Принцип единой ответственности ===
+// class Character {
+//     #inventory = [];
+//     #health = 10;
+//     pickItem(item) {
+//         this.#inventory.push(item);
+//     }
+//     recieveDamage(damage) {
+//         this.#health -= damage;
+//     }
+//     // Если реализовать методы save и load тут, то мы нарушим принцип S
+// }
+// class DB {
+//     save(item) {
+//         localStorage.setItem('char', this);
+//     }
+//     load() {
+//         // ...
+//     }
+// }
 
-    pickItem(item) {
-        this.#inventory.push(item);
-    }
+// // === Принцип открытости и закрытости ===
+// class Treasure {
+//     value = 0;
+// }
+// class Coin extends Treasure {
+//     value = 1;
+// }
+// class Crystal extends Treasure {
+//     value = 10;
+// }
+// class Briliant extends Treasure {
+//     value = 20;
+// }
+// class Inventory {
+//     #score;
+//     pick(treasure) {
+//         if (treasure instanceof Coin) {
+//             this.#score += treasure.value;
+//         }
+//     }
+// }
 
-    recieveDamage(damage) {
-        this.#health -= damage;
-    }
+// // === Принцип подстановки Барбабы Лисков ===
+// class User {
+//     #role = 'user';
+//     getRole() {
+//         return this.#role;
+//     }
+// }
+// class Admin extends User {
+//     #role = ['user', 'admin'];
+//     getRole() {
+//         return this.#role.join(', ');
+//     }
+// }
+// function logRole(user) {
+//     console.log('Role: ' + user.getRole().toUpperCase());
+// }
+// logRole(new User());
+// logRole(new Admin());
 
-    saveCharacter() {
-        localStorage.setItem('char', this);
-    }
+// // === Принцип разделения интерфейса ===
+// class Weapon {
+//     dealDamage() {}
+// }
+// class Rifle extends Weapon {
+//     shoot() {
+//         this.dealDamage();
+//     }
+// }
+// class Sword extends Weapon {
+//     strike() {
+//         this.dealDamage();
+//     }
+// }
 
-    loadCharacter() {
-        localStorage.getItem('char');
-    }
-}
-
-
-// === Принцип открытости и закрытости ===
-// === Принцип подстановки Барбабы Лисков ===
-// === Принцип разделения интерфейса ===
-// === Принцип инверсии зависимостей ===
+// // === Принцип инверсии зависимостей ===
+// class DB {
+//     save(items) {
+//         console.log(`Saved: ${items}`);
+//     }
+// }
+// class Mongo extends DB {
+//     save(items) {
+//         console.log(`Saved to Mongo: ${items}`);
+//     }
+// }
+// class ToDoList {
+//     db;
+//     items = [1, 2, 3];
+//     constructor(db) {
+//         this.db = db;
+//     }
+//     saveToDB() {
+//         this.db.save(this.items);
+//     }
+// }
+// const list1 = new ToDoList(new DB());
+// list1.saveToDB();
+// const list2 = new ToDoList(new Mongo());
+// list2.saveToDB();
